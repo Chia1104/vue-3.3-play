ab27e7ab253fcb534f1d8934b86c72fc574b574e
Chia1104 <yuyuchia7423@gmail.com>
2023-05-16 01:41:15 +0800

    init

---
+++ src/components/Input.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { defineModel } from "vue";
+const modelValue = defineModel()
+</script>
+
+<template>
+  <input v-model="modelValue" type="text" />
+</template>
+
+<style scoped>
+input {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 0.5em;
+  font-size: 1em;
+  width: 100%;
+}
+</style>
